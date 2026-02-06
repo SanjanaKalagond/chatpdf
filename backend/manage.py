88f887af7161cd83c17b8f2793b32ae28f7a18a3
SanjanaKalagond <155650195+SanjanaKalagond@users.noreply.github.com>
2026-02-06 17:11:08 +0530

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -5,17 +5,14 @@ from pathlib import Path
 
 
 def main():
-    # ADD THIS (repo root)
     ROOT_DIR = Path(__file__).resolve().parent.parent
     sys.path.append(str(ROOT_DIR))
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatpdf_backend.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
         raise
     execute_from_command_line(sys.argv)
-
 
 if __name__ == "__main__":
     main()
