f79620660b3a59937c941a5dd0288612ae142d54
SanjanaKalagond <155650195+SanjanaKalagond@users.noreply.github.com>
2026-02-06 11:30:53 +0000

    Add wsgi.py configuration

--- backend/chatpdf_backend/wsgi.py
+++ backend/chatpdf_backend/wsgi.py
@@ -1,10 +1,3 @@
-
-"""
-WSGI config for chatpdf_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
